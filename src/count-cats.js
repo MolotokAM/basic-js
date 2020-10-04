d04ee85067b591ce6d7c4089f4ac65a2bb928d87
Artur <2329494@gmail.com>
2020-10-04 21:43:00 +0300

    init: test completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,5 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(/* matrix */) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  module.exports = function countCats(/* matrix */) {
+    throw new CustomError('Not implemented');
+    for (let i = 0 ; countCats.length; i++) {
+      for (let j = i ; i++; j++) {
+        if(countCats[i][j] === '^^'){
+          number ++;
+          i++;
+        }
+      }
+    console.log(number); 
+    }
+    return number;
+  };
 };
